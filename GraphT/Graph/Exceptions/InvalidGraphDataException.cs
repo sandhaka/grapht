167a9367a30115bcac0bed0e316f48fdb0dec857
Matteo Tosato <matteo.tosato7@gmail.com>
2025-02-18 16:08:48 +0100

    Ext validation

--- GraphT/Graph/Exceptions/InvalidGraphDataException.cs
+++ GraphT/Graph/Exceptions/InvalidGraphDataException.cs
@@ -1,14 +1,13 @@
-using System.Diagnostics.CodeAnalysis;
-
 namespace GraphT.Graph.Exceptions;
 
 public sealed class InvalidGraphDataException<T> : Exception
 {
-    [SetsRequiredMembers]
+    public InvalidGraphDataException() { }
+
     public InvalidGraphDataException(T item) : base($"Invalid graph data: {item}")
     {
         Item = item;
     }
     
-    public required T Item { get; init; }
+    public T? Item { get; }
 }
--- GraphT/Models/Validators/GraphModelValidatorsExtensions.cs
+++ GraphT/Models/Validators/GraphModelValidatorsExtensions.cs
@@ -3,9 +3,9 @@ using GraphT.Models.Abstractions;
 
 namespace GraphT.Models.Validators;
 
-public static class GraphModelValidatorsExtensions
+internal static class GraphModelValidatorsExtensions
 {
-    internal static void Validate<T>(this IGraphListModel<T> model) where T : IEquatable<T>
+    public static void Validate<T>(this IGraphListModel<T> model) where T : IEquatable<T>
     {
         // Check if all edges are valid
         var edges = model.AdjacencyList;
@@ -16,8 +16,12 @@ public static class GraphModelValidatorsExtensions
         }
     }
     
-    internal static void Validate<T>(this IGraphMatrixModel<T> model) where T : IEquatable<T>
+    public static void Validate<T>(this IGraphMatrixModel<T> model) where T : IEquatable<T>
     {
-        // TODO
+        // Check first two dimensions
+        if (model.Matrix.GetLength(1) != model.Nodes.Length)
+            throw new InvalidGraphDataException<T>();
+        if (model.Matrix.GetLength(0) != model.Nodes.Length)
+            throw new InvalidGraphDataException<T>();
     }
 }
